b728eee144f2760438f9886531497b70f7cf5f76
unknown <ezejudith02@gmail.com>
2022-06-23 13:08:30 +0100

    first javascript commit

---
+++ basicb.js
@@ -0,0 +1,8 @@
+let Name = 'Chidera_';
+let height = '153cm or 5ft';
+let country = 'Nigeria';
+
+
+document.write("My name is: " + Name + "<br>"
+    + "My height is: " + height + "<br>"
+    + "My country is: " + country);
